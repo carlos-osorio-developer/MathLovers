8208d9bc6fcaa0cd1be0662eef3f4e11775029ce
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-16 17:43:09 -0500

    Add calculator component and first styling rules

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './calculator.css';
 
 class Value extends React.Component {
   constructor(props) {
@@ -7,8 +8,43 @@ class Value extends React.Component {
   }
 
   render() {
-    return (<div>
-      <p>{this.state.value}</p>
+    return (<div className='calculator'>
+        <div className='display'><p>{this.state.value}</p></div>
+        <div className='panel'>
+          <div className='buttons'>
+            <div className='row'>
+              <input type='button' value='AC'></input>
+              <input type='button' value='+/-'></input>
+              <input type='button' value='%'></input>
+            </div>
+            <div className='row'>
+              <input type='button' value='7'></input>
+              <input type='button' value='8'></input>
+              <input type='button' value='9'></input>
+            </div>
+            <div className='row'>
+              <input type='button' value='4'></input>
+              <input type='button' value='5'></input>
+              <input type='button' value='6'></input>
+            </div>
+            <div className='row'>
+              <input type='button' value='1'></input>
+              <input type='button' value='2'></input>
+              <input type='button' value='3'></input>
+            </div>
+            <div className='row'>
+              <input type='button' value='0' className='double-size'></input>
+              <input type='button' value='.'></input>
+            </div>
+          </div>
+          <div className='operations'>
+            <input type='button' value='/'></input>
+            <input type='button' value='*'></input>
+            <input type='button' value='-'></input>
+            <input type='button' value='+'></input>
+            <input type='button' value='='></input>
+          </div>
+        </div>
       </div>
     );
   }
