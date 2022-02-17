899b029de7cecf4947e27d6f71ed55c1979e4028
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-16 19:17:32 -0500

    Fix destructuring state assignment error

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,53 +1,55 @@
 import React from 'react';
 import './calculator.scss';
 
-class Value extends React.Component {
+const Calculator = class Value extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: 0 };
   }
 
   render() {
-    return (<div className='calculator'>
-        <div className='display'><p>{this.state.value}</p></div>
-        <div className='panel'>
-          <div className='buttons'>
-            <div className='row'>
-              <input type='button' value='AC'></input>
-              <input type='button' value='+/-'></input>
-              <input type='button' value='%'></input>
+    const { value } = this.state;
+    return (
+      <div className="calculator">
+        <div className="display"><p>{value}</p></div>
+        <div className="panel">
+          <div className="buttons">
+            <div className="row">
+              <input type="button" value="AC" />
+              <input type="button" value="+/-" />
+              <input type="button" value="%" />
             </div>
-            <div className='row'>
-              <input type='button' value='7'></input>
-              <input type='button' value='8'></input>
-              <input type='button' value='9'></input>
+            <div className="row">
+              <input type="button" value="7" />
+              <input type="button" value="8" />
+              <input type="button" value="9" />
             </div>
-            <div className='row'>
-              <input type='button' value='4'></input>
-              <input type='button' value='5'></input>
-              <input type='button' value='6'></input>
+            <div className="row">
+              <input type="button" value="4" />
+              <input type="button" value="5" />
+              <input type="button" value="6" />
             </div>
-            <div className='row'>
-              <input type='button' value='1'></input>
-              <input type='button' value='2'></input>
-              <input type='button' value='3'></input>
+            <div className="row">
+              <input type="button" value="1" />
+              <input type="button" value="2" />
+              <input type="button" value="3" />
             </div>
-            <div className='row'>
-              <input type='button' value='0' className='double-size'></input>
-              <input type='button' value='.'></input>
+            <div className="row">
+              <input type="button" value="0" className="double-size" />
+              <input type="button" value="." />
             </div>
           </div>
-          <div className='operations'>
-            <input type='button' value='/'></input>
-            <input type='button' value='*'></input>
-            <input type='button' value='-'></input>
-            <input type='button' value='+'></input>
-            <input type='button' value='=' className='equal'></input>
+          <div className="operations">
+            <input type="button" value="/" />
+            <input type="button" value="*" />
+            <input type="button" value="-" />
+            <input type="button" value="+" />
+            <input type="button" value="=" className="equal" />
           </div>
         </div>
       </div>
     );
   }
-}
+};
 
-export default Value;
+export default Calculator;
