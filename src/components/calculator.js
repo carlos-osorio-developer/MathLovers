2344440f90591e17dfa07878163e3e57c96287aa
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-19 20:44:46 -0500

    Refactor to add functions with hooks instead of class component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,67 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './calculator.scss';
 import Key from './key';
 import calculate from '../logic/calculate';
 import Display from './display';
 
-const Calculator = class Value extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { total: '0', next: '', operation: null };
-    this.clickHandler = this.clickHandler.bind(this);
-  }
+const Calculator = ({ settings: initialSettings }) => {
+  const [settings, setSettings] = useState(initialSettings);
 
-  clickHandler(e) {
-    const obj = { ...this.state };
-    const melojuma = calculate(obj, e.target.value);
-    this.setState(melojuma);
-    console.log(obj);
-    console.log(melojuma);
-  }
+  const clickHandler = (e) => {
+    const calc = calculate({ ...settings }, e.target.value);
+    setSettings({ ...calc });
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator">
-        <Display total={total} next={next} operation={operation} />
-        <div className="panel">
-          <div className="buttons">
-            <div className="row">
-              <Key clickEvent={this.clickHandler} value="AC" />
-              <Key clickEvent={this.clickHandler} value="+/-" />
-              <Key clickEvent={this.clickHandler} value="%" />
-            </div>
-            <div className="row">
-              <Key clickEvent={this.clickHandler} value="7" />
-              <Key clickEvent={this.clickHandler} value="8" />
-              <Key clickEvent={this.clickHandler} value="9" />
-            </div>
-            <div className="row">
-              <Key clickEvent={this.clickHandler} value="4" />
-              <Key clickEvent={this.clickHandler} value="5" />
-              <Key clickEvent={this.clickHandler} value="6" />
-            </div>
-            <div className="row">
-              <Key clickEvent={this.clickHandler} value="1" />
-              <Key clickEvent={this.clickHandler} value="2" />
-              <Key clickEvent={this.clickHandler} value="3" />
-            </div>
-            <div className="row">
-              <Key clickEvent={this.clickHandler} value="0" className="double-size" />
-              <Key clickEvent={this.clickHandler} value="." />
-            </div>
+  return (
+    <div className="calculator">
+      <Display total={settings.total} next={settings.next} operation={settings.operation} />
+      <div className="panel">
+        <div className="buttons">
+          <div className="row">
+            <Key clickEvent={clickHandler} value="AC" />
+            <Key clickEvent={clickHandler} value="+/-" />
+            <Key clickEvent={clickHandler} value="%" />
           </div>
-          <div className="operations">
-            <Key clickEvent={this.clickHandler} value="/" />
-            <Key clickEvent={this.clickHandler} value="*" />
-            <Key clickEvent={this.clickHandler} value="-" />
-            <Key clickEvent={this.clickHandler} value="+" />
-            <Key clickEvent={this.clickHandler} value="=" className="equal" />
+          <div className="row">
+            <Key clickEvent={clickHandler} value="7" />
+            <Key clickEvent={clickHandler} value="8" />
+            <Key clickEvent={clickHandler} value="9" />
+          </div>
+          <div className="row">
+            <Key clickEvent={clickHandler} value="4" />
+            <Key clickEvent={clickHandler} value="5" />
+            <Key clickEvent={clickHandler} value="6" />
+          </div>
+          <div className="row">
+            <Key clickEvent={clickHandler} value="1" />
+            <Key clickEvent={clickHandler} value="2" />
+            <Key clickEvent={clickHandler} value="3" />
+          </div>
+          <div className="row">
+            <Key clickEvent={clickHandler} value="0" className="double-size" />
+            <Key clickEvent={clickHandler} value="." />
           </div>
         </div>
+        <div className="operations">
+          <Key clickEvent={clickHandler} value="/" />
+          <Key clickEvent={clickHandler} value="*" />
+          <Key clickEvent={clickHandler} value="-" />
+          <Key clickEvent={clickHandler} value="+" />
+          <Key clickEvent={clickHandler} value="=" className="equal" />
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+Calculator.defaultProps = { settings: { total: '0', next: '', operation: null } };
+
+Calculator.propTypes = {
+  settings: PropTypes.shape(
+    {
+      total: PropTypes.string,
+      next: PropTypes.string,
+      operation: PropTypes.string,
+    },
+  ),
 };
 
 export default Calculator;
