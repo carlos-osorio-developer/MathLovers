0a47ad2c6a91c93aeafc0a0d7a2f6b0775247f3f
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-24 20:43:42 -0500

    Add styles for home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 const Home = () => (
-  <div>
-    <h1>Home</h1>
-    <p>
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      marginTop: '6rem',
+      textAlign: 'center',
+    }}
+  >
+    <h1 style={{
+      fontSize: '4rem',
+      fontWeight: '600',
+      marginBottom: '2rem',
+    }}
+    >
+      Home
+    </h1>
+    <p
+      style={{
+        fontSize: '1.5rem',
+        fontWeight: '600',
+        marginBottom: '2rem',
+        width: '50%',
+      }}
+    >
       Mathematical beauty is the aesthetic pleasure typically derived from the abstractness,
       purity, simplicity, depth or orderliness of mathematics.
       Mathematicians often express this pleasure by describing mathematics as beautiful.
