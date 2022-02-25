cb63fc746bb643d90ce42da87a8ce5c8dfc03b91
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-24 22:03:11 -0500

    Add tests for operate.js functions

---
+++ src/__tests__/operate.test.js
@@ -0,0 +1,37 @@
+import operate from '../logic/operate';
+
+it('returns a string', () => {
+  const result = operate(1, 2, '+');
+  expect(typeof result).toBe('string');
+}
+);
+
+it('should add two numbers', () => {
+    const result = operate(1, 2, '+');
+    expect(result).toBe('3');
+  }
+);
+
+it('should subtract two numbers', () => {
+    const result = operate(1, 2, '-');
+    expect(result).toBe('-1');
+  }
+);
+
+it('should multiply two numbers', () => {
+    const result = operate(-3, 2, '*');
+    expect(result).toBe('-6');
+  }
+);
+
+it('should divide two numbers', () => {
+    const result = operate(1, 2, '/');
+    expect(result).toBe('0.5');
+  }
+);
+
+it('should return the modulus of two numbers', () => {
+    const result = operate(1, 2, '%');
+    expect(result).toBe('1');
+  }
+);
