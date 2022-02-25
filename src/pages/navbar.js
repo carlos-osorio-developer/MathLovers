85ce0b6ccaa6eebf91e337f969b080db9f8e385e
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-24 19:48:17 -0500

    Add Link to logo

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Link, Outlet } from 'react-router-dom';
 import { BsGithub } from 'react-icons/bs';
 import styles from './navbar.module.scss';
 
 const Navbar = () => (
   <div>
     <nav className={styles.navbar}>
-      <div className={styles.logo}>
+      <Link to="/" className={styles.logo}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Math.svg/2048px-Math.svg.png" alt="logo" />
-      </div>
+      </Link>
       <div className={styles.navLinks}>
         <div />
         <ul className={styles.navList}>
           <li className={styles.navItem}>
-            <NavLink to="/" activeStyle={{ color: 'red' }}>Home</NavLink>
+            <NavLink to="/">Home</NavLink>
           </li>
           <li className={styles.navItem}>
-            <NavLink to="/calculator" activeStyle={{ color: 'red' }}>Calculator</NavLink>
+            <NavLink to="/calculator">Calculator</NavLink>
           </li>
         </ul>
         <a href="https://github.com/carlos-osorio-developer/MathLovers">
