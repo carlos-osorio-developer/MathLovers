5860b3c290e6a71a0542564ef6b9655bc9f5acdb
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-24 20:52:19 -0500

    Add message in calculator page

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -14,41 +14,64 @@ const Calculator = ({ settings: initialSettings }) => {
   };
 
   return (
-    <div className="calculator">
-      <Display total={settings.total} next={settings.next} operation={settings.operation} />
-      <div className="panel">
-        <div className="buttons">
-          <div className="row">
-            <Key clickEvent={clickHandler} value="AC" />
-            <Key clickEvent={clickHandler} value="+/-" />
-            <Key clickEvent={clickHandler} value="%" />
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '100vh',
+        width: '80%',
+        margin: '0 auto',
+      }}
+    >
+      <div style={{ width: '50%' }}>
+        <p
+          style={{
+            fontSize: '2rem',
+            fontWeight: '600',
+            marginBottom: '2rem',
+            lineHeight: '1em',
+          }}
+        >
+          { "Let's math! =>" }
+        </p>
+      </div>
+      <div className="calculator">
+        <Display total={settings.total} next={settings.next} operation={settings.operation} />
+        <div className="panel">
+          <div className="buttons">
+            <div className="row">
+              <Key clickEvent={clickHandler} value="AC" />
+              <Key clickEvent={clickHandler} value="+/-" />
+              <Key clickEvent={clickHandler} value="%" />
+            </div>
+            <div className="row">
+              <Key clickEvent={clickHandler} value="7" />
+              <Key clickEvent={clickHandler} value="8" />
+              <Key clickEvent={clickHandler} value="9" />
+            </div>
+            <div className="row">
+              <Key clickEvent={clickHandler} value="4" />
+              <Key clickEvent={clickHandler} value="5" />
+              <Key clickEvent={clickHandler} value="6" />
+            </div>
+            <div className="row">
+              <Key clickEvent={clickHandler} value="1" />
+              <Key clickEvent={clickHandler} value="2" />
+              <Key clickEvent={clickHandler} value="3" />
+            </div>
+            <div className="row">
+              <Key clickEvent={clickHandler} value="0" className="double-size" />
+              <Key clickEvent={clickHandler} value="." />
+            </div>
           </div>
-          <div className="row">
-            <Key clickEvent={clickHandler} value="7" />
-            <Key clickEvent={clickHandler} value="8" />
-            <Key clickEvent={clickHandler} value="9" />
+          <div className="operations">
+            <Key clickEvent={clickHandler} value="/" />
+            <Key clickEvent={clickHandler} value="*" />
+            <Key clickEvent={clickHandler} value="-" />
+            <Key clickEvent={clickHandler} value="+" />
+            <Key clickEvent={clickHandler} value="=" className="equal" />
           </div>
-          <div className="row">
-            <Key clickEvent={clickHandler} value="4" />
-            <Key clickEvent={clickHandler} value="5" />
-            <Key clickEvent={clickHandler} value="6" />
-          </div>
-          <div className="row">
-            <Key clickEvent={clickHandler} value="1" />
-            <Key clickEvent={clickHandler} value="2" />
-            <Key clickEvent={clickHandler} value="3" />
-          </div>
-          <div className="row">
-            <Key clickEvent={clickHandler} value="0" className="double-size" />
-            <Key clickEvent={clickHandler} value="." />
-          </div>
-        </div>
-        <div className="operations">
-          <Key clickEvent={clickHandler} value="/" />
-          <Key clickEvent={clickHandler} value="*" />
-          <Key clickEvent={clickHandler} value="-" />
-          <Key clickEvent={clickHandler} value="+" />
-          <Key clickEvent={clickHandler} value="=" className="equal" />
         </div>
       </div>
     </div>
